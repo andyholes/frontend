427803a436af1cdaa6ce7cc234b5332e45abb48d
andyholes <andyholesdev@gmail.com>
2023-08-24 16:48:36 -0300

    add filter by tag

--- frontend/src/component/NoteList.js
+++ frontend/src/component/NoteList.js
@@ -1,13 +1,14 @@
-import Note from "./Note";
 import React, { useState, useEffect } from "react";
 import axios from "axios";
 import './NoteList.css';
+import Note from "./Note";
 import NoteForm from "./NoteForm";
 
 function NotesList() {
     const [notes, setNotes] = useState([]);
     const [isArchived, setArchived] = useState(false);
     const [showForm, setShowForm] = useState(false);
+    const [selectedTag, setSelectedTag] = useState("All"); // Default to showing all notes
 
     const baseUrl = `http://localhost:8080/notes`;
 
@@ -17,6 +18,8 @@ function NotesList() {
         .then((response) => setNotes(response.data))
         .catch((error) => console.log(error))
     ;}, [baseUrl]);
+
+    const allTags = Array.from(new Set(notes.flatMap(note => note.tags))).sort();
 
     const cancelForm = () => setShowForm(false);
 
@@ -28,11 +31,11 @@ function NotesList() {
         axios
         .post(baseUrl, newNote)
         .then((response) => {
-          console.log("Created successfully");
-          addToList(response.data);
-          })
-          .catch((error) => console.log(error))
-          setShowForm(false);
+            console.log("Created successfully");
+            addToList(response.data);
+        })
+        .catch((error) => console.log(error))
+        setShowForm(false);
     };
 
     const addToList = (note) => {
@@ -44,7 +47,7 @@ function NotesList() {
         console.log("A note was removed from the list");
     };
 
-    const updateArchived = (id) =>{
+    const updateArchived = (id) => {
         const noteIndex = notes.findIndex((note) => note.id === id);
         const updatedList = [...notes];
         updatedList[noteIndex].archived = !updatedList[noteIndex].archived;
@@ -63,34 +66,54 @@ function NotesList() {
         setArchived(!isArchived);
     };
 
-    const notePlaceholder = {id:0, title:'', content:'', archived: false, tags:[]}
+    const emptyNote = {id:0, title:'', content:'', archived: false, tags:[]}
+
+    const filteredNotes = notes.filter((note) => {
+        if (selectedTag === "All") {
+            return true; 
+        } else {
+            return note.tags.includes(selectedTag);
+        }
+    });
 
     return (
-    <>
-    <div className="main-title">
-        <h1>My notes</h1>
-        <button className="create-button" onClick={handleCreation}>Create note</button>
-        <button className="toggle-archived" onClick={showArchived}>{isArchived ? 
-        '< Go back to unarchived notes' :
-        'Archived notes'}</button>
-    </div>
-    <div className="note-list">
-        {notes
-        .filter((note) => note.archived === isArchived)
-        .map((note) => (
-        <Note key={note.id} 
-        note={note} 
-        updateList={updateList} 
-        updateNote={updateNote} 
-        updateArchived={updateArchived}/>
-        ))}
+        <>
+            <div className="main-title">
+                <h1>My notes</h1>
+                <button className="create-button" onClick={handleCreation}>Create note</button>
+                <button className="toggle-archived" onClick={showArchived}>{isArchived ?
+                    '< Go back to unarchived notes' :
+                    'Archived notes'}</button>
+                <select
+                    onChange={(e) => setSelectedTag(e.target.value)}
+                    value={selectedTag}
+                    className="tag-filter"
+                >
+                    <option value="All">all-tags</option>
+                    {allTags.map((tag) => (
+                        <option key={tag} value={tag}>
+                            {tag}
+                        </option>
+                    ))}
+                </select>
+            </div>
+            <div className="note-list">
+                {filteredNotes
+                    .filter((note) => note.archived === isArchived)
+                    .map((note) => (
+                        <Note key={note.id}
+                            note={note}
+                            updateList={updateList}
+                            updateNote={updateNote}
+                            updateArchived={updateArchived}/>
+                    ))}
 
-        {showForm && 
-        <NoteForm note={notePlaceholder}
-        onConfirm={confirmCreation} 
-        onCancel={cancelForm}/>}
-    </div>
-    </>
+                {showForm &&
+                <NoteForm note={emptyNote}
+                          onConfirm={confirmCreation}
+                          onCancel={cancelForm}/>}
+            </div>
+        </>
     );
 }
 
